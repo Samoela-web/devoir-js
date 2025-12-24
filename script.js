67c579d73215976994a898cc6011552419a86264
Samoela-web <randriasam.pro@gmail.com>
2025-12-24 15:23:44 +0300

    Add files via upload
    Ajout du devoir JavaScript

---
+++ script.js
@@ -0,0 +1,19 @@
+let marque = "Toyota"
+let modèle = "Corolla 12 trek"
+let année = "2022"
+let prix = 30000
+let Voiture = "Voiture Marque de " + marque + ", Modele " + modèle + ", Annee " + année  + ", Prix " + prix + "$"
+
+console.log(Voiture);
+
+
+variable = "Peugeot"
+modèle = "306 "
+année = "1993"
+prix = prix / 2
+
+let nouveauVoiture = "Voiture de Marque " + variable + ", Modele " + modèle + ", Annee " + année + ", Prix " + prix + "$"
+
+console.log(nouveauVoiture);
+
+
